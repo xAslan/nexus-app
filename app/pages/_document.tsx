ed11c5e2a53a111dcba50127e49f2dcb575a5663
maotora <maotoramakweba@live.com>
2020-12-14 14:44:21 +0300

    Prefix to styled-components hydration issues

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,25 +1,44 @@
-import { Document, Html, DocumentHead, Main, BlitzScript /*DocumentContext*/ } from "blitz"
+import { Document, Html, DocumentHead, Main, BlitzScript, DocumentContext } from "blitz"
+import { ServerStyleSheet } from "styled-components"
 
 class MyDocument extends Document {
-  // Only uncomment if you need to customize this behaviour
-  // static async getInitialProps(ctx: DocumentContext) {
-  //   const initialProps = await Document.getInitialProps(ctx)
-  //   return {...initialProps}
-  // }
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 
   render() {
     return (
       <Html lang="en">
-        <DocumentHead>
-          {/* Heap Snippet */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        {/* Heap Snippet */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
               heap.load("3104354919");`,
-            }}
-          />
-        </DocumentHead>
+          }}
+        />
+        <DocumentHead />
         <body>
           <Main />
           <BlitzScript />
