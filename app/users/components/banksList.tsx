67dcce7ffdc897b8b4f99f296abe403cdb19934d
maotora <maotoramakweba@live.com>
2021-04-05 15:39:03 +0300

    Fixed Holdings Loading

--- app/users/components/banksList.tsx
+++ app/users/components/banksList.tsx
@@ -1,38 +1,44 @@
-import { Input, List, Button, Row, Col, Card } from "antd"
+import { Skeleton, Input, List, Button, Row, Col, Card } from "antd"
 import * as styled from "app/users/components/styles"
 import { useAggregates } from "app/users/components/dashboardCtx"
 
 const BanksList = (props) => {
   const { holdings } = useAggregates()
 
-  return (
-    <styled.AccountsCard title="Accounts" bordered={false}>
-      <List
-        itemLayout="horizontal"
-        dataSource={holdings}
-        loading={holdings.length < 0 ? true : false}
-        renderItem={({ institution, fiatAmount, amount, asset }) => (
-          <List.Item>
-            <styled.BanksList>
-              <div>
-                <strong> {institution.name} </strong>
-                <strong> ${fiatAmount} </strong>
-              </div>
+  const renderList = (holdings = []) => {
+    console.log(holdings)
+    return (
+      <styled.AccountsCard title="Accounts" bordered={false}>
+        <List
+          itemLayout="horizontal"
+          dataSource={holdings}
+          renderItem={({ institution, fiatAmount, amount, asset }) => (
+            <Skeleton loading={holdings.length > 0 ? false : true} active avatar>
+              <List.Item>
+                <styled.BanksList>
+                  <div>
+                    <strong> {institution.name} </strong>
+                    <strong> ${fiatAmount} </strong>
+                  </div>
 
-              <div>
-                <span> {amount} </span>
-                <span> {asset.symbol} </span>
-              </div>
-            </styled.BanksList>
-          </List.Item>
-        )}
-      />
+                  <div>
+                    <span> {amount} </span>
+                    <span> {asset.symbol} </span>
+                  </div>
+                </styled.BanksList>
+              </List.Item>
+            </Skeleton>
+          )}
+        />
 
-      <styled.CenteredButton>
-        <Button block> Link Account </Button>
-      </styled.CenteredButton>
-    </styled.AccountsCard>
-  )
+        <styled.CenteredButton>
+          <Button block> Link Account </Button>
+        </styled.CenteredButton>
+      </styled.AccountsCard>
+    )
+  }
+
+  return renderList(holdings)
 }
 
 export default BanksList
--- app/users/components/totalAmount.tsx
+++ app/users/components/totalAmount.tsx
@@ -1,22 +1,33 @@
 import * as styled from "app/users/components/styles"
+import { Skeleton } from "antd"
 import { useAggregates } from "app/users/components/dashboardCtx"
 
 const TotalAmount = (props) => {
   const { holdingsSum } = useAggregates()
 
-  return (
-    <styled.TotalAmountCard bordered={false}>
-      <p>
-        <strong>$&nbsp;{holdingsSum.toLocaleString("en-US", { maximumFractionDigits: 2 })}</strong>
-        <span> +4% </span>
-      </p>
+  const renderSum = (sum = 0) => {
+    if (sum > 0) {
+      return (
+        <styled.TotalAmountCard bordered={false}>
+          <p>
+            <strong>
+              $&nbsp;{holdingsSum.toLocaleString("en-US", { maximumFractionDigits: 2 })}
+            </strong>
+            <span> +4% </span>
+          </p>
 
-      <p>
-        <strong style={{ textDecoration: "strike" }}>&pound; 8,000</strong>
-        <span> 30 days </span>
-      </p>
-    </styled.TotalAmountCard>
-  )
+          <p>
+            <strong style={{ textDecoration: "strike" }}>&pound; 8,000</strong>
+            <span> 30 days </span>
+          </p>
+        </styled.TotalAmountCard>
+      )
+    }
+
+    return <Skeleton active={true} loading={true} />
+  }
+
+  return renderSum(holdingsSum)
 }
 
 export default TotalAmount
