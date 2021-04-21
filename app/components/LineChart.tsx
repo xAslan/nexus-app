f69e0a204e74256d30408008620b8e9e19ea0913
maotora <maotoramakweba@live.com>
2021-04-22 02:06:03 +0300

    Functional Linechart

--- app/components/LineChart.tsx
+++ app/components/LineChart.tsx
@@ -1,24 +1,24 @@
-import React from "react"
+import { useState, useEffect } from "react"
 import { Card } from "antd"
-import "antd/dist/antd.css"
 import { Line } from "react-chartjs-2"
+import _ from "lodash"
 
-let today = new Date()
+const today = new Date()
 
-let lastYearStart = new Date(today.getFullYear() - 1, 0, 1)
-let lastYearEnd = new Date(today.getFullYear(), 0, 1)
+const lastYearStart = new Date(today.getFullYear() - 1, 0, 1)
+const lastYearEnd = new Date(today.getFullYear(), 0, 1)
 
-let lastMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
-let lastMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 1, 1)
+const lastMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
+const lastMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 1, 1)
 
-let threeMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
-let threeMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 3, 1)
+const threeMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
+const threeMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 3, 1)
 
-let sixMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
-let sixMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 6, 1)
+const sixMonthEnd = new Date(today.getFullYear(), today.getMonth(), 1)
+const sixMonthStart = new Date(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth() - 6, 1)
 
-let lastWeekStart = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 6)
-let lastWeekEnd = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
+const lastWeekStart = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 6)
+const lastWeekEnd = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
 
 const options = {
   tooltips: {
@@ -55,21 +55,15 @@ const options = {
   },
 }
 
-class LineChart extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      chartRange: "All",
-    }
-  }
+const LineChart = (props) => {
+  const [chartRange, setChartRange] = useState("All")
+  const [chartLabel, setChartLabel] = useState([])
+  const [chartData, setChartData] = useState([])
+  const handleDropDownChange = ({ target }) => setChartRange(target.value)
 
-  handleChange = (e) => {
-    this.setState({ chartRange: e.target.value })
-  }
-
-  RangeDropdownButtons = (e) => {
+  const rangeDropdownButtons = (e) => {
     return (
-      <select onBlur={this.handleChange}>
+      <select onBlur={(e) => handleDropDownChange(e)}>
         <option value="All">All</option>
         <option value="lastWeek">7 Days</option>
         <option value="lastMonth">1 Month</option>
@@ -80,140 +74,116 @@ class LineChart extends React.Component {
     )
   }
 
-  render() {
-    this.chartLabelName = []
-    let chartLabelInProgress = []
+  const getChartData = (accountBalance) => {
+    return _.reduce(
+      accountBalance,
+      (acc, currentAccount, Idx) => {
+        const d = new Date(currentAccount.timestamp)
+        // let dateFromLowest = d.getDay() + ' ' + d.toLocaleString('default', {month: 'long'}) + ' ' + d.getFullYear();
 
-    this.chartData = []
-    let chartDataInProgress = []
-
-    // let sortDateFromLowest = this.props.valueOfAccount.sort((a, b) => a.timestamp - b.timestamp);
-
-    for (let i = 0; i < this.props.valueOfAccount.length; i++) {
-      var d = new Date(this.props.valueOfAccount[i].timestamp)
-      // let dateFromLowest = d.getDay() + ' ' + d.toLocaleString('default', {month: 'long'}) + ' ' + d.getFullYear();
-
-      if (this.state.chartRange === "All") {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      } else if (
-        this.state.chartRange === "lastWeek" &&
-        this.props.valueOfAccount[i].timestamp >= lastWeekStart.getTime() &&
-        this.props.valueOfAccount[i].timestamp < lastWeekEnd.getTime()
-      ) {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      } else if (
-        this.state.chartRange === "lastMonth" &&
-        this.props.valueOfAccount[i].timestamp >= lastMonthStart.getTime() &&
-        this.props.valueOfAccount[i].timestamp < lastMonthEnd.getTime()
-      ) {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      } else if (
-        this.state.chartRange === "threeMonth" &&
-        this.props.valueOfAccount[i].timestamp >= threeMonthStart.getTime() &&
-        this.props.valueOfAccount[i].timestamp < threeMonthEnd.getTime()
-      ) {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      } else if (
-        this.state.chartRange === "sixMonth" &&
-        this.props.valueOfAccount[i].timestamp >= sixMonthStart.getTime() &&
-        this.props.valueOfAccount[i].timestamp < sixMonthEnd.getTime()
-      ) {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      } else if (
-        this.state.chartRange === "lastYear" &&
-        this.props.valueOfAccount[i].timestamp >= lastYearStart.getTime() &&
-        this.props.valueOfAccount[i].timestamp < lastYearEnd.getTime()
-      ) {
-        chartLabelInProgress.push(
-          d.getDate() +
-            " " +
-            d.toLocaleString("default", { month: "short" }) +
-            " " +
-            d.getFullYear()
-        )
-        chartDataInProgress.push(this.props.valueOfAccount[i].value)
-
-        this.chartLabelName = chartLabelInProgress
-        this.chartData = chartDataInProgress
-      }
-    }
-
-    this.data = (canvas) => {
-      const ctx = canvas.getContext("2d")
-      const gradient = ctx.createLinearGradient(0, 0, 0, 400)
-      gradient.addColorStop(0, "rgba(62, 222, 134, 0.8)")
-      gradient.addColorStop(1, "rgba(162, 219, 187, 0.35)")
-
-      return {
-        labels: this.chartLabelName,
-        datasets: [
-          {
-            label: "Amount",
-            data: this.chartData,
-            fill: true,
-            backgroundColor: gradient,
-            borderColor: gradient,
-          },
-        ],
-      }
-    }
-
-    return (
-      <Card title={this.props.title} extra={this.RangeDropdownButtons()}>
-        <Line data={this.data} options={options} />
-      </Card>
+        if (chartRange === "All") {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        } else if (
+          chartRange === "lastWeek" &&
+          currentAccount.timestamp >= lastWeekStart.getTime() &&
+          currentAccount.timestamp < lastWeekEnd.getTime()
+        ) {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        } else if (
+          chartRange === "lastMonth" &&
+          currentAccount.timestamp >= lastMonthStart.getTime() &&
+          currentAccount.timestamp < lastMonthEnd.getTime()
+        ) {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        } else if (
+          chartRange === "threeMonth" &&
+          currentAccount.timestamp >= threeMonthStart.getTime() &&
+          currentAccount.timestamp < threeMonthEnd.getTime()
+        ) {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        } else if (
+          chartRange === "sixMonth" &&
+          currentAccount.timestamp >= sixMonthStart.getTime() &&
+          currentAccount.timestamp < sixMonthEnd.getTime()
+        ) {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        } else if (
+          chartRange === "lastYear" &&
+          currentAccount.timestamp >= lastYearStart.getTime() &&
+          currentAccount.timestamp < lastYearEnd.getTime()
+        ) {
+          return acc.concat({
+            chartData: currentAccount.value,
+            chartLabel: `${d.getDate()} ${d.toLocaleString("default", {
+              month: "short",
+            })} ${d.getFullYear()}`,
+          })
+        }
+        return acc
+      },
+      []
     )
   }
+
+  useEffect(() => {
+    const chartDataArr = getChartData(props.valueOfAccount)
+    const chartLabel = _.flatMap(chartDataArr, ({ chartLabel, chartData }) => chartLabel)
+    const chartData = _.flatMap(chartDataArr, ({ chartLabel, chartData }) => chartData)
+
+    setChartData(chartData)
+    setChartLabel(chartLabel)
+  }, [props.valueOfAccount, chartRange])
+
+  const data = (canvas) => {
+    const ctx = canvas.getContext("2d")
+    const gradient = ctx.createLinearGradient(0, 0, 0, 400)
+    gradient.addColorStop(0, "rgba(62, 222, 134, 0.8)")
+    gradient.addColorStop(1, "rgba(162, 219, 187, 0.35)")
+
+    return {
+      labels: chartLabel,
+      datasets: [
+        {
+          label: "Amount",
+          data: chartData,
+          fill: true,
+          backgroundColor: gradient,
+          borderColor: gradient,
+        },
+      ],
+    }
+  }
+
+  return (
+    <Card title={props.title} extra={rangeDropdownButtons()}>
+      <Line data={data} options={options} />
+    </Card>
+  )
 }
 
 export default LineChart
