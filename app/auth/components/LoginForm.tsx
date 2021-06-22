569ad261bd150ff0cee61398278a212746085793
maotora <maotoramakweba@live.com>
2021-06-22 14:49:04 +0300

    Tailwinded Auth

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,126 +1,214 @@
-import React from "react"
+import { useState } from "react"
 import { useMutation, Link } from "blitz"
 import login from "app/auth/mutations/login"
-import { MdEmail, MdLock } from "react-icons/md"
-import { Button, Row, Col, Form, Input, message, Checkbox } from "antd"
-import * as styled from "app/components/styles"
+import { Field, Form } from "react-final-form"
 
 type LoginFormProps = {
   onSuccess?: () => void
 }
 
-export const LoginForm = (props: LoginFormProps) => {
+const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
+  const [showToastr, setShowToastr] = useState(false)
+  const [message, setMessage] = useState({})
+
   const handleSubmit = async (values) => {
     try {
-      await loginMutation({ email: values.email, password: values.password })
+      const [email, password] = [values.email, values.password]
+      await loginMutation({ email: email.trim(), password: password.trim() })
       props.onSuccess && props.onSuccess()
     } catch (error) {
       if (error.name === "AuthenticationError") {
-        return message.error("Sorry, those credentials are invalid")
+        setShowToastr(true)
+        setMessage({ error: "Invalid authentication credentials" })
       } else {
-        return message.error("Sorry, We have an unexpected Error!")
+        setShowToastr(true)
+        setMessage({ error: "Something happened" })
       }
     }
   }
 
-  const formItemLayout = {
-    wrapperCol: {
-      xs: { span: 24 },
-    },
-    labelCol: {
-      xs: { span: 24 },
-    },
-  }
-
   return (
-    <styled.FormWrapper>
-      <header>
-        <h2> Login </h2>
-      </header>
-      <Row type="flex" justify="center">
-        <Col xs={23} lg={18}>
-          <Form
-            name="Login_Form"
-            onFinish={handleSubmit}
-            {...formItemLayout}
-            labelAlign="left"
-            onFinishFailed={(error) => {
-              console.log(error)
-              return message.error("Sorry, We have an unexpected Error!")
-            }}
-          >
-            <SignInFields {...props} />
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="mx-auto h-12 w-auto" src="/logo.png" alt="Workflow" />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
 
-            <ActionButtons {...props} />
-          </Form>
-        </Col>
-      </Row>
-    </styled.FormWrapper>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <Form
+            onSubmit={handleSubmit}
+            render={({ handleSubmit }) => (
+              <form className="space-y-6">
+                <Field name="email" component="input">
+                  {({ input, meta }) => (
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Email address
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          {...input}
+                          required
+                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Field>
+
+                <Field name="password" component="input">
+                  {({ input, meta }) => (
+                    <div>
+                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                        Password
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          {...input}
+                          autoComplete="current-password"
+                          required
+                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Field>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember_me"
+                      name="remember_me"
+                      type="checkbox"
+                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    />
+                    <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a href="#" className="font-medium text-green-600 hover:text-green-500">
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+                {showToastr && message.error && (
+                  <p className="text-sm text-red-500"> {message.error} </p>
+                )}
+                <div>
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+            )}
+          />
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">Or continue with</span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign in with Facebook</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign in with Twitter</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign in with GitHub</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mt-5">
+              <div className="flex items-center">
+                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                  Don't have a Nexus account yet?
+                </label>
+              </div>
+
+              <div className="text-md">
+                <Link href="/signup">
+                  <a className="font-bold text-green-600 underline hover:text-green-500">
+                    Sign Up!
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default LoginForm
-
-const SignInFields = (props) => {
-  return (
-    <>
-      <Form.Item
-        name="email"
-        label="Email"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-      >
-        <Input size="large" prefix={<MdEmail />} />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input.Password size="large" prefix={<MdLock />} type="password" placeholder="Password" />
-      </Form.Item>
-    </>
-  )
-}
-
-const ActionButtons = (props) => {
-  const [rememberUser, setRememberUser] = React.useState(true)
-
-  return (
-    <Form.Item>
-      <Checkbox onChange={() => setRememberUser(!rememberUser)}>Remember me</Checkbox>
-      <Link href="forgot">
-        <a>Forgot password</a>
-      </Link>
-      <Row justify="center">
-        <Col xs={22} lg={18} xxl={16}>
-          <Button type="primary" size="large" block htmlType="submit">
-            <strong>Login</strong>
-          </Button>
-          <p>
-            {" "}
-            Not registered yet? &nbsp;
-            <Link href="/signup">
-              <a>Sign Up</a>
-            </Link>
-          </p>
-        </Col>
-      </Row>
-    </Form.Item>
-  )
-}
--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -1,126 +1,234 @@
-import { useMutation, useRouter, Link } from "blitz"
+import { useState } from "react"
+import { useMutation, Link } from "blitz"
 import signup from "app/auth/mutations/signup"
-import { Button, message, Form, Input, Row, Col, Checkbox } from "antd"
-import { MdEmail, MdPerson } from "react-icons/md"
-import * as styled from "app/components/styles"
+import { Field, Form } from "react-final-form"
 
-const SignupForm = (props) => {
-  const router = useRouter()
+type SignupProps = {
+  onSuccess?: () => void
+}
+
+const SignupForm = (props: SignupProps) => {
   const [signupMutation] = useMutation(signup)
-
-  const formItemLayout = {
-    wrapperCol: {
-      xs: { span: 24 },
-    },
-    labelCol: {
-      xs: { span: 24 },
-    },
-  }
+  const [showToastr, setShowToastr] = useState(false)
+  const [message, setMessage] = useState({})
 
   const handleSubmit = async (values) => {
     try {
       delete values.agreement
       await signupMutation({ email: values.email, name: values.name, password: values.password })
-      router.back()
+      props.onSuccess && props.onSuccess()
 
       message.success("User created!")
     } catch (error) {
       if (error.code === "P2002" && error.meta?.target?.includes("email")) {
         // This error comes from Prisma
-        message.error("Email is already being used.")
+        setShowToastr(true)
+        setMessage({ error: "Email is already being used." })
       } else {
-        console.log(error.toString())
-        message.error(`Something is wrong ${error.toString()}`)
+        setShowToastr(true)
+        setMessage({ error: `Something is wrong ${error.toString()}` })
       }
     }
   }
 
   return (
-    <styled.FormWrapper>
-      <header>
-        <h2> Sign Up </h2>
-      </header>
-      <main>
-        <Row justify="center">
-          <Col xs={24}>
-            <Form {...formItemLayout} onFinish={handleSubmit} labelAlign="left">
-              <RegistrationFields {...props.form} />
-              <ActionButons {...props} />
-            </Form>
-          </Col>
-        </Row>
-      </main>
-    </styled.FormWrapper>
-  )
-}
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="mx-auto h-12 w-auto" src="/logo.png" alt="Workflow" />
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Create your Nexus Account
+        </h2>
+      </div>
 
-const ActionButons = (props) => {
-  return (
-    <Form.Item>
-      <Row justify="center">
-        <Col xs={22}>
-          <Button block type="primary" size="large" htmlType="submit">
-            <strong>Register</strong>
-          </Button>
-          <p>
-            {" "}
-            Already a member? &nbsp;
-            <Link href="login">
-              <a> Sign in </a>
-            </Link>
-          </p>
-        </Col>
-      </Row>
-    </Form.Item>
-  )
-}
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <Form
+            onSubmit={handleSubmit}
+            render={({ handleSubmit }) => (
+              <form className="space-y-6">
+                <Field name="name" component="input">
+                  {({ input, meta }) => (
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Names
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="name"
+                          name="name"
+                          type="text"
+                          {...input}
+                          required
+                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Field>
 
-const RegistrationFields = (props) => {
-  return (
-    <>
-      <Form.Item
-        label="Name"
-        rules={[{ required: true, message: "Please input your name!" }]}
-        name="name"
-      >
-        <Input size="large" suffix={<MdPerson />} />
-      </Form.Item>
-      <Form.Item
-        label="E-mail"
-        name="email"
-        rules={[
-          {
-            type: "email",
-            message: "Not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-      >
-        <Input size="large" suffix={<MdEmail />} />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        hasFeedback
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password size="large" id="password" />
-      </Form.Item>
-      <Form.Item name="agreement">
-        <Checkbox>
-          I have read the <a href="/terms">agreement</a>
-        </Checkbox>
-        ,
-      </Form.Item>
-    </>
+                <Field name="email" component="input">
+                  {({ input, meta }) => (
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Email address
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          {...input}
+                          required
+                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Field>
+
+                <Field name="password" component="input">
+                  {({ input, meta }) => (
+                    <div>
+                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                        Password
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          {...input}
+                          autoComplete="current-password"
+                          required
+                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Field>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember_me"
+                      name="remember_me"
+                      type="checkbox"
+                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    />
+                    <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a href="#" className="font-medium text-green-600 hover:text-green-500">
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+                {showToastr && message.error && (
+                  <p className="text-sm text-red-500"> {message.error} </p>
+                )}
+                <div>
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            )}
+          />
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">Or continue with</span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign up with Facebook</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign up with Twitter</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                </a>
+              </div>
+
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                >
+                  <span className="sr-only">Sign up with GitHub</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mt-5">
+              <div className="flex items-center">
+                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                  Already have a Nexus Account?
+                </label>
+              </div>
+
+              <div className="text-md">
+                <Link href="/login">
+                  <a className="font-bold text-green-600 underline hover:text-green-500">Login!</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
